93760fe5387f0f73162bfea29bc6737c6455b429
Brian Ashton <ashtobd@gmail.com>
2023-06-27 16:26:01 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form')
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    buildToDo(e.target["new-task-description"].value)
+    form.reset()
+  })
 });
+
+function buildToDo(tasks) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  li.textContent = tasks + " "
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
